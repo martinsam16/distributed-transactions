231abe9567ee616d0c5529ceef64920a33037678
Martín Alexis Samán Arata <martinsam1610@gmail.com>
2023-05-03 01:13:56 -0500

    feat: initialize project

---
+++ catalogo-productos/catalogproducts/src/main/java/pe/martinsam/catalogproducts/CatalogproductsApplication.java
@@ -0,0 +1,13 @@
+package pe.martinsam.catalogproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogproductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogproductsApplication.class, args);
+    }
+
+}
---
+++ catalogo-productos/catalogproducts/src/test/java/pe/martinsam/catalogproducts/CatalogproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.martinsam.catalogproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogproductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
